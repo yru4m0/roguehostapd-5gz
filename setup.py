43974d0e22792ae103ac34b00d4e48ef69216be4
Amaury <amaury.lecoq@tuta.io>
2026-07-27 02:47:57 +0200

    Rewrite setup.py to be self-contained, no imports from package being built
    setup.py previously imported from roguehostapd.buildutil which failed
    during pip's get_requires_for_build_wheel step. Now the C extension
    build logic is inlined in setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,88 @@
+#!/usr/bin/env python
 """
 Module for setup hostapd shared library
 """
 
-from setuptools import setup
+import os
+import sys
+import shutil
+from textwrap import dedent
+import tempfile
+import distutils.sysconfig
+import distutils.ccompiler
+from distutils.errors import CompileError, LinkError
+from setuptools import Extension, setup
 from distutils.command.build_ext import build_ext
-import roguehostapd.buildutil.buildcommon as buildcommon
-import roguehostapd.buildutil.buildexception as buildexception
+
+HOSTAPD_DIR = os.path.dirname(os.path.abspath(__file__))
+HOSTAPD_SRC = os.path.join(HOSTAPD_DIR, "roguehostapd", "hostapd", "src")
+HOSTAPD_UTILS = os.path.join(HOSTAPD_SRC, "utils")
+LIB_NL3_PATH = "/usr/include/libnl3"
+LIB_SSL_PATH = "/usr/include/openssl"
+SHARED_LIB_PATH = "roguehostapd.hostapd.hostapd"
+
+HOSTAPD_MACROS = [
+    ("CONFIG_DRIVER_NL80211", None),
+    ("CONFIG_LIBNL32", None),
+    ("__linux__", None),
+]
+
+
+def get_all_source_files():
+    src_dir = os.path.join(HOSTAPD_DIR, "roguehostapd", "hostapd", "src")
+    sources = []
+    for root, dirs, files in os.walk(src_dir):
+        for f in files:
+            if f.endswith(".c"):
+                sources.append(os.path.join(root, f))
+    return sources
+
+
+LIBNL_CODE = dedent("""
+#include <netlink/netlink.h>
+#include <netlink/genl/genl.h>
+int main(int argc, char* argv[])
+{
+   struct nl_msg *testmsg;
+   testmsg = nlmsg_alloc();
+   nlmsg_free(testmsg);
+   return 0;
+}
+""")
+
+OPENSSL_CODE = dedent("""
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+int main(int argc, char* argv[])
+{
+    SSL_load_error_strings();
+    return 0;
+}
+""")
+
+
+def check_required_library(libname, libraries=None, include_dir=None):
+    build_success = True
+    tmp_dir = tempfile.mkdtemp(prefix="tmp_" + libname + "_")
+    bin_file_name = os.path.join(tmp_dir, "test_" + libname)
+    file_name = bin_file_name + ".c"
+    code = LIBNL_CODE if libname == "netlink" else OPENSSL_CODE
+    with open(file_name, "w") as filep:
+        filep.write(code)
+    compiler = distutils.ccompiler.new_compiler()
+    distutils.sysconfig.customize_compiler(compiler)
+    try:
+        compiler.link_executable(
+            compiler.compile([file_name], include_dirs=include_dir),
+            bin_file_name,
+            libraries=libraries,
+        )
+    except (CompileError, LinkError):
+        build_success = False
+    finally:
+        shutil.rmtree(tmp_dir)
+    return build_success
+
 
 # define project information
 NAME = "roguehostapd"
@@ -17,37 +94,37 @@ DESCRIPTION = "Hostapd wrapper for hostapd"
 URL = "https://github.com/wifiphisher/roguehostapd"
 AUTHOR = "Anakin"
 
-try:
-    EXT_MODULE = buildcommon.get_extension_module()
-    setup(
-        name=NAME,
-        packages=PACKAGES,
-        package_dir=PACKAGE_DIR,
-        package_data=PACKAGE_DATA,
-        version=VERSION,
-        description=DESCRIPTION,
-        url=URL,
-        author=AUTHOR,
-        install_requires=[],
-        zip_safe=False,
-        cmdclass={"build_ext": build_ext},
-        ext_modules=EXT_MODULE,
-    )
-except buildexception.SharedLibMissError as exobj:
-    RED = "\033[31m"
-    WHITE = "\033[0m"
+if not check_required_library("netlink", ["nl-3", "nl-genl-3"], [LIB_NL3_PATH]):
     print(
-        "["
-        + RED
-        + "!"
-        + WHITE
-        + "] "
-        + (
-            "The development package for "
-            + exobj.libname
-            + " is missing. Please download it and restart the compilation."
-            "If you are on Debian-based system: 'apt-get install{}'.".format(
-                "".join(" " + package for package in exobj.packages)
-            )
-        )
+        "[!] The development package for netlink is missing. Please download it and restart the compilation. apt-get install libnl-3-dev libnl-genl-3-dev"
     )
+    sys.exit(1)
+
+if not check_required_library("openssl", ["ssl"], [LIB_SSL_PATH]):
+    print(
+        "[!] The development package for openssl is missing. Please download it and restart the compilation. apt-get install libssl-dev"
+    )
+    sys.exit(1)
+
+ext_module = Extension(
+    SHARED_LIB_PATH,
+    define_macros=HOSTAPD_MACROS,
+    libraries=["rt", "ssl", "crypto", "nl-3", "nl-genl-3"],
+    sources=get_all_source_files(),
+    include_dirs=[HOSTAPD_SRC, HOSTAPD_UTILS, LIB_NL3_PATH],
+)
+
+setup(
+    name=NAME,
+    packages=PACKAGES,
+    package_dir=PACKAGE_DIR,
+    package_data=PACKAGE_DATA,
+    version=VERSION,
+    description=DESCRIPTION,
+    url=URL,
+    author=AUTHOR,
+    install_requires=[],
+    zip_safe=False,
+    cmdclass={"build_ext": build_ext},
+    ext_modules=[ext_module],
+)
